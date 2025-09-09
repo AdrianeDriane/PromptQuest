d8733ea895f067354666e5a885427bfeb4a1f83d
AdrianeDriane <dilaoaparre@gmail.com>
2025-09-09 10:29:28 +0800

    new setup html css js tailwind

--- client/src/utils/getFingerprint.ts
+++
@@ -1,7 +0,0 @@
-import FingerprintJS from "@fingerprintjs/fingerprintjs";
-
-export const getFingerprint = async (): Promise<string> => {
-  const fp = await FingerprintJS.load();
-  const result = await fp.get();
-  return result.visitorId;
-};
